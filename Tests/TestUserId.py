00671c9a7158d4c32d5165189d1145a105894596
Nikita Kuznetsov <n.kuznetsov@wanna.fashion>
2022-09-25 23:08:43 +0300

    add test_response_schema for user id

--- Tests/TestUserId.py
+++ Tests/TestUserId.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-
+from pytest_schema import schema
 
 class TestUserId:
     url = 'https://hr-challenge.interactivestandard.com/api/test/user/'
@@ -9,3 +9,22 @@ class TestUserId:
     def test_positive_status_code(self, user_id):
         response = requests.get(self.url + user_id)
         assert response.status_code == 200
+
+    def test_response_schema(self):
+        expected_schema = {
+            "success": bool,
+            "errorCode": int,
+            "errorMessage": str,
+            "result":
+            {
+                "id": str,
+                "name": str,
+                "gender": str,
+                "age": int,
+                "city": str,
+                "registrationDate": str
+            }
+        }
+        existing_user_id = '5'
+        response = requests.get(self.url + existing_user_id)
+        assert schema(expected_schema) == response.json()
