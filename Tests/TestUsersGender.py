7bda01249e072b2e4b71309bd6fa4c3d05bcaa44
Nikita Kuznetsov <n.kuznetsov@wanna.fashion>
2022-09-25 19:51:48 +0300

    add test for content without result

--- Tests/TestUsersGender.py
+++ Tests/TestUsersGender.py
@@ -9,4 +9,9 @@ class TestUserGender:
     def test_positive_status_code(self, gender, expected):
         response = requests.get(self.url, params={'gender': gender})
         assert response.status_code == expected
-        
+            @pytest.mark.parametrize('gender, param_for_check, expected',
+                             [('male', 'success', True), ('male', 'errorCode', 0), ('male', 'errorMessage', None),
+                              ('female', 'success', True), ('female', 'errorCode', 0), ('female', 'errorMessage', None)])
+    def test_positive_response_content_except_result(self, gender,  param_for_check, expected):
+        response = requests.get(self.url, params={'gender': gender})
+        assert response.json().get(param_for_check) == expected
