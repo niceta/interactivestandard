76b91d13014245951f5cfd3c95b715745fbcbbe2
Nikita Kuznetsov <n.kuznetsov@wanna.fashion>
2022-09-25 19:12:19 +0300

    rename variable

--- Tests/TestUsersGender.py
+++ Tests/TestUsersGender.py
@@ -5,8 +5,8 @@ import requests
 class TestUserGender:
     url = 'https://hr-challenge.interactivestandard.com/api/test/users'
 
-    @pytest.mark.parametrize("test_input, expected", [('male', 200), ('female', 200)])
-    def test_positive_status_code(self, test_input, expected):
-        response = requests.get(self.url, params={'gender': test_input})
+    @pytest.mark.parametrize("gender, expected", [('male', 200), ('female', 200)])
+    def test_positive_status_code(self, gender, expected):
+        response = requests.get(self.url, params={'gender': gender})
         assert response.status_code == expected
-    
+        
