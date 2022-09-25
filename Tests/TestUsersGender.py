c5d08a594b9fe956d1d7e8191e56ae6b127bc042
Nikita Kuznetsov <n.kuznetsov@wanna.fashion>
2022-09-25 23:03:57 +0300

    add test json schema

--- Tests/TestUsersGender.py
+++ Tests/TestUsersGender.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+from pytest_schema import schema
 
 
 class TestUserGender:
@@ -9,6 +10,17 @@ class TestUserGender:
     def test_positive_status_code(self, gender, expected):
         response = requests.get(self.url, params={'gender': gender})
         assert response.status_code == expected
+
+    @pytest.mark.parametrize('gender', ['male', 'female'])
+    def test_response_schema(self, gender):
+        expected_schema = {
+            "success": bool,
+            "errorCode": int,
+            "errorMessage": str,
+            "result": list
+        }
+        response_json = requests.get(self.url, params={'gender': gender}).json()
+        assert schema(expected_schema) == response_json
 
     @pytest.mark.parametrize('gender, param_for_check, expected',
                              [('male', 'success', True), ('male', 'errorCode', 0), ('male', 'errorMessage', None),
